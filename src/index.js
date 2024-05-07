85cc1ec16da3615369aac80b9ae92cfabb6a597c
dennis <dennisdust2@gmail.com>
2024-05-07 15:45:19 -0400

    Started the basic HTML outline of the webpage

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
 import './style.css'
 import _ from "lodash";
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join('Hi ');
-    element.classList.add('hello');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
